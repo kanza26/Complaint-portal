b927c384e2e9d623f48e93d2cb12ad70b3bd68f1
kanza <kanzafatima9211@gmail.com>
2024-04-23 05:00:36 -0700

    initial commit

---
+++ front-end/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import TypewriterEffectDemo from './components/homepage/homepage1.jsx'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TypewriterEffectDemo/>
+  </React.StrictMode>,
+)
---
+++ front-end/tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        Poppins: ["Poppins"]
+      }
+    },
+    screens: {
+      'sm': {'min': '300px', 'max': '600px'},
+      // => @media (min-width: 640px and max-width: 767px) { ... }
+
+      'md': {'min': '610px', 'max': '1023px'},
+      // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+      'lg': {'min': '1024px', 'max': '1279px'},
+      // => @media (min-width: 1024px and max-width: 1279px) { ... }
+
+      'xl': {'min': '1280px', 'max': '1535px'},
+      // => @media (min-width: 1280px and max-width: 1535px) { ... }
+
+      '2xl': {'min': '1536px'},
+      // => @media (min-width: 1536px) { ... }
+      
+  },
+  plugins: [],
+}
+}
